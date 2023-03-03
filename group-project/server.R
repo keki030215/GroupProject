612decc2b0b8f405043405e307a3f2434ba9b7a7
keki030215 <keliuhan215@gmail.com>
2023-03-03 12:48:31 -0800

    shiny added

---
+++ group-project/server.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
+
+server <- function(input, output) {}
+
+shinyServer(server)
---
+++ group-project/ui.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
+
+ui <- fluidPage()
+
+shinyUI(ui)
