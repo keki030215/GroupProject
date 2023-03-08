602d1dbeb715b60c87c026c04b2d5777f0fa1136
keki030215 <keliuhan215@gmail.com>
2023-03-07 19:17:41 -0800

    code change

--- group-project/student-merge.R
+++ group-project/student-merge.R
@@ -9,3 +9,8 @@ d3 <- merge(d1, d2, by = c("school","sex","age","address",
 print(nrow(d3)) # 382 students
 
 row.names(d3) <- as.character(1:382)
+
+d3 %>% 
+  group_by(school) %>% 
+  ggplot(aes(x = health.x, fill = school)) +
+  geom_histogram(position = "dodge2")
